700e8c9be46c7ff08cf9938c76bf5d0d5cb9ef95
Florin Barbu <florin.barbu@oath.com>
2019-02-20 11:43:20 +0200

    paralel run with grid on docker

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # selenium_docker_grid
+
+Docker setup from terminal:
+docker run -d -P --name selenium-hub selenium/hub
+docker run -d -P --link selenium-hub:hub selenium/node-chrome-debug
+docker run -d -P --link selenium-hub:hub selenium/node-firefox-debug
---
+++ src/test/java/rough/GridTest.java
@@ -0,0 +1,43 @@
+package rough;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class GridTest {
+
+public WebDriver driver=null;	
+	
+	@Parameters("browser")
+	@Test()
+	public void googleTest(String b) throws MalformedURLException, InterruptedException {
+		System.out.println("Google " + b);
+		DesiredCapabilities cap = null;
+		if(b.equals("firefox")) {
+			cap = DesiredCapabilities.firefox();
+			cap.setBrowserName("firefox");
+			cap.setPlatform(Platform.ANY);
+		} else if(b.equals("chrome")) {
+			cap = DesiredCapabilities.chrome();
+			cap.setBrowserName("chrome");
+			cap.setPlatform(Platform.ANY);
+		}
+		
+		driver = new RemoteWebDriver(new URL("http://192.168.99.100:32768/wd/hub"), cap);
+		
+		driver.get("https://www.google.ro/");
+		driver.findElement(By.name("q")).sendKeys("Hello "+b);
+		Thread.sleep(5000);
+		
+		driver.quit();
+		
+		
+	}
+}
